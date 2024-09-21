127acaec1d446a7aafa03349deaa0d546873c57d
Soul pc <119080077+soulpc@users.noreply.github.com>
2024-09-21 23:14:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,9 +42,9 @@ class evamaria(Client):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ['12618934'])
+API_HASH = environ['49aacd0bc2f8924add29fb02e20c8a16']
+BOT_TOKEN = environ['5288373674:AAFq1O6nB_SP39_Xoj6vz5z3AOf7NBBc7Gg']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
